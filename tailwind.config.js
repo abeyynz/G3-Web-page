947726e77d7b0937666d938481c05d59f49e3d28
triaindri <triaindri983@gmail.com>
2024-11-02 19:46:54 +0700

    mengubah file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}"
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
   ],
   theme: {
     extend: {},
